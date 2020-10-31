f0819fa4fe028deead3cb573486a83e8f866496c
Agbo Boniface Ebuka <boniface.ebuka@gmail.com>
2020-10-31 05:14:37 +0100

    Edited the two functions to convert argument to small letters

--- cipher.php
+++ cipher.php
@@ -4,7 +4,7 @@
 		$str_arr1 = ['a', 'o', 'p', 't', 's', 'j', 'm'];
 		$str_arr2 = ['e', 'u' ,'b', 'd' ,'c' ,'g' ,'n'];
 		
-		$strreplace = $str;
+		$strreplace = strtolower($str);
 
 		for ($i=0; $i <= count($str_arr1) -1; $i++) { 
 			$strreplace = str_replace($str_arr1[$i], $str_arr2[$i], $strreplace);
@@ -18,7 +18,7 @@
 		$str_arr1 = ['a', 'o', 'p', 't', 's', 'j', 'm'];
 		$str_arr2 = ['e', 'u' ,'b', 'd' ,'c' ,'g' ,'n'];
 		
-		$strreplace = $str;
+		$strreplace = strtolower($str);
 
 		for ($i=0; $i <= count($str_arr1) -1; $i++) { 
 			$strreplace = str_replace($str_arr2[$i],$str_arr1[$i], $strreplace);
